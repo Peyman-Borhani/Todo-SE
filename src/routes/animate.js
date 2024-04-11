318f891aa0c9fa0c420ecf6a9777b504a9f99660
peyman-borhani <peyman.borhani@gmail.com>
2024-04-11 18:03:30 +0430

    +Moved: animate code from +page => animate.js

---
+++ src/routes/animate.js
@@ -0,0 +1,17 @@
+import  {crossfade}     from  'svelte/transition';
+import  {quintOut }     from  'svelte/easing';
+
+export const [send, receive] = crossfade(
+{   duration: d => Math.sqrt(d * 400), 
+    fallback(node, params) { 
+        const style = getComputedStyle(node);
+        const transform = style.transform === 'none' ? '' : style.transform;
+
+        return {duration: 900,
+                easing  : quintOut,
+                css     : t => `transform: ${transform} 
+                                scale(${t});
+                                opacity  : ${t} `
+        };
+    }
+});
