2f767afbd5d225e64d7e5acc738e989f1e8c2558
peyman-borhani <peyman.borhani@gmail.com>
2024-04-22 16:43:55 +0430

    +Fixed: issues to make Runes work in js +Fix:  logic: check  props valid

--- src/lib/store.svelte.js
+++ src/lib/store.svelte.js
@@ -1,6 +1,7 @@
 import  {Map}  from  'svelte/reactivity';
 export  {make_Store}
 
+
 let qN= 1;  // que number
 
 const getTime = (t = new Date().toISOString())=> 
@@ -23,37 +24,25 @@ const
 
 function  make_Store()
 {
-    const   Data = $state(new Map());
-    //_________initializing Data______________
-    function   init  (todos=false,  dones=false)
-    {
-        if(D.o.length>0 || D.one.length>0)  return; // "error: initialized already";
-    //assign todos/dones params if it exist && valid else default values  
-        D.o   =Array.from( (Array.isArray(todos) && todos.length>0)
-                            ? todos :def_todos);
-        D.one =Array.from( (Array.isArray(dones) && dones.length>0)
-                            ? dones :def_dones );
-        qN = 1;
-        D.one.forEach( dn=> insert(dn, 'done') );
-        D.o.forEach(  td=> insert(td, 'todo') );
-    //for(let d in D) d==='o'? console.log('logArr : ',D[d])  :{};
-    }
-    //____________________Add new item__________________
-    function  insert(inp='',    typ='todo',
-                     timer='_', now = getTime())
-    {   //__Check/set the type of input
-        if (typ!=='todo' || typ!=='done')  typ= false;
-        // check if date/time input is valid [min, hr, day, month, year]
-        // * TODO: format the timer ************************************
-        if (timer.length<22 || timer!=='_')  timer= false
-        //test numbers || typeof inp!=='string')
-        if(!typ || !timer || inp.length<1 || inp==='' 
-                || now.length<22)  return "bad input"; 
+    let   Data = $state(new Map());
+  
+    //let complete = false;
+  //____________________Add new item__________________
+     function insert (inp='', typ='todo', timer='_', now = getTime().toString()){
+        //__Check/set the type of input
+        if(typ!=='todo' && typ!=='done') typ=false;
+        // * TODO: check if date/time input is valid [min, hr, day, month, year]
+        //  format the timer ************************************
+        if (timer!=='_' && timer.length<5)  timer= false; 
+        // * Todo: test numbers || typeof inp!=='string' now format)
+        if (!typ || !timer || inp.length<1 || inp==='' || now.length<5)
+            {console.log('bad input'); return}
         // Update D.o/D.one array  //remove if not needed
         (typ==='todo')  ? D.o   =[inp, ...D.o]   
                         : D.one =[inp, ...D.one];
         // nake a unique id    // C.randomUUID();
-        let id = URL.createObjectURL(new Blob([])).slice(-36);   
+        let id = URL.createObjectURL(new Blob([])).slice(-36); //36  
+         
         // setting a new Map item
         Data.set( id, new Map([ ['done',  (typ==='done')],
                                 ['item',  inp],
@@ -63,17 +52,31 @@ function  make_Store()
                                 ['timer', timer],
                                 ['tasks', false] ])  );
         URL.revokeObjectURL(id);
+    }  
+    //_________initializing Data______________
+    function  init (todos=false,  dones=false){
+        
+       // if(D.o.length<1 || D.one.length<1){ // "error: if initialized already";
+        //assign todos/dones params if it exist && valid else default values  
+            D.o   =Array.from( (Array.isArray(todos) && todos.length>0)
+                                ? todos :def_todos);
+            D.one =Array.from( (Array.isArray(dones) && dones.length>0)
+                                ? dones :def_dones );
+        //} //qN = 1;
+        D.one.forEach( dn=> insert(dn, 'done') );
+        D.o.forEach(  td=> insert(td, 'todo') );
+        //console.log('Daaaaaaaataaaa: ',Data.size);
     }
     //___________________Data Tools_________________________
     const  
-        remove  = q=> Data.delete(q),
-        mark    = q=> Data[q].done.set(true),
+        remove  = id=> Data.delete(id),
+        mark    = id=> Data[id].done.set(!Data[id].done),
         sort    = x=>  new Map([...x].sort()),
         rev     = x=>  new Map([...x].reverse()),
         // Using Map.groupBy to categorize items based on quantity
         sortBy  = (id, x)=> Map.groupBy(Data=> Data.get(id).get(x)),
-        log     = ()=> {for(let d of Data.keys()) console.table(d)};
-
+        log     = ()=> {for(let d of Data.keys()) console.log(d)};
+        
     return { 
             get Data() {return Data},   
             init,   insert,  remove,  mark,
@@ -86,12 +89,11 @@ function  updateID (qID=1) 	{
         D.o.forEach(t=> (!t.done)? t.qID = qID++ : {})
         D.o.forEach(t=> (t.done) ? t.qID = qID++ : {})
 }//__________________________________________________________
-
 // Using Map.groupBy to categorize items based on quantity
 const result = 
 	Map.groupBy(store,({quantity})=> quantity<9? 'restock' :'ok');
 
-*/
+
 function save(file_name) {
         //name= task_name = document.getElementsByTagName('VAR').item(0).innerText; 
         let  S  =localStorage;
@@ -105,3 +107,4 @@ function load(file_name) {
         LS = localStorage.getItem(LS); 
         console.log(LS)
 }
+*/
