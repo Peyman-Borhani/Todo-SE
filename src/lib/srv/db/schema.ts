10e976c7689f0d408619b7b761f2c5723cd6a9d7
peyman-borhani <peyman.borhani@gmail.com>
2024-05-01 21:31:14 +0430

    +Add: schema.ts => defined db schema

---
+++ src/lib/srv/db/schema.ts
@@ -0,0 +1,31 @@
+import  {sql}   from 'drizzle-orm';
+import  {integer,  sqliteTable,  
+         text,     uniqueIndex}  from   'drizzle-orm/sqlite-core';
+
+export const usersTable = sqliteTable(
+  'users', 
+  {
+	id: text('id').primaryKey().notNull(),
+
+	name: text('name').notNull(),
+
+	email: text('email').notNull().unique(),
+
+	password: text('password').notNull(),
+
+	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
+  },  
+  (users)=> ({uid: uniqueIndex('uid').on(users.name)})
+);
+
+export const  usersSessionsTable = sqliteTable(
+  'users_sessions', 
+  {
+    id: text('id').primaryKey().notNull(),
+
+    userId: text('user_id').notNull().references(()=> usersTable.id),
+    
+    expiresAt: integer('expires_at').notNull()
+});
+
+export type UserInsertSchema = typeof usersTable.$inferInsert;
