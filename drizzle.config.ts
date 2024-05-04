16657546cad7136c21e517b9ce804df992300612
peyman-borhani <peyman.borhani@gmail.com>
2024-05-04 15:15:33 +0430

    +Fix: typo: .src => ./src

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import  {defineConfig}  from 'drizzle-kit';
 
 export default defineConfig({
   schema:   './src/lib/srv/db/schema.ts',
-  out:      '.src/lib/srv/db/out',
+  out:      './src/lib/srv/db/out',
   driver:   'better-sqlite',
   dbCredentials: { url: './src/lib/srv/db/todo-se-db.sql'}, //'localDB/sql.db' },
     //connectionString: process.env.DB_URL,
