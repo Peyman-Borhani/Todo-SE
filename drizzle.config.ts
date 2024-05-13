5b0896b769bad47a7f3fe38c653908989b0e795e
peyman-borhani <peyman.borhani@gmail.com>
2024-05-13 20:07:43 +0430

    +Update: +Fix: drizzle.config.js => config correction and completed (now works)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import  {defineConfig}  from 'drizzle-kit';
+import  {defineConfig}  from  'drizzle-kit';
+//import  {ENP}      from    '$env/static/private';
 
 export default defineConfig({
   schema:   './src/lib/srv/db/schema.ts',
   out:      './src/lib/srv/db/out',
-  driver:   'better-sqlite',
-  dbCredentials: { url: './src/lib/srv/db/todo-se-db.sql'}, //'localDB/sql.db' },
+  dialect:  'sqlite',
+  dbCredentials: {url: './src/lib/srv/db/todo-se.db'}, //'localDB/sql.db' },
     //connectionString: process.env.DB_URL,
-  
   verbose: true,
   strict: true,
-})
+});
