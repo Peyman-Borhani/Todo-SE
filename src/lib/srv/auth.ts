fdf8571a3f06f9ada3c61db5d8029befe6450407
peyman-borhani <peyman.borhani@gmail.com>
2024-05-21 10:51:13 +0430

    +Add:  export DB schema => in schema.ts
    +code: uniform view+BSR

--- src/lib/srv/auth.ts
+++ src/lib/srv/auth.ts
@@ -7,21 +7,22 @@ import  Database        from    'better-sqlite3';
 
 import {dev}        from  '$app/environment';
 
-const sqliteDB = Database(':memory:');
-const db = drizzle(sqliteDB);
+const  sqliteDB = Database(':memory:');
+const  db       = drizzle(sqliteDB);
 
-const userT = sqliteTable(  'user', 
+const  userT  =sqliteTable( 'user', 
 {
     id:     text('id').notNull().primaryKey(),
     password: text('password').notNull(),
-    accessAt: text('access_at').notNull().default(sql`CURRENT_TIMESTAMP`)
+    accessAt: text('access_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`)
 });
 
-const sessionT = sqliteTable(  'session', 
-{   id: text('id').notNull().primaryKey(),
+const  sessionT  =sqliteTable( 'session', 
+{   
+    id:     text('id').notNull().primaryKey(),
     userId: text('user_id').notNull().references(()=>userT.id),
 	expiresAt: integer('expires_at').notNull()
 });
 
-const adapter = new DrizzleSQLiteAdapter(db, sessionT, userT);
-
+const adapter =new DrizzleSQLiteAdapter(db, sessionT, userT);
--- src/lib/srv/db/schema.ts
+++ src/lib/srv/db/schema.ts
@@ -1,31 +1,25 @@
+export  {userT,  sessionT}
+export type  UserInsertSchema  =typeof userT.$inferInsert;
+
 import  {sql}   from 'drizzle-orm';
 import  {integer,  sqliteTable,  
          text,     uniqueIndex}  from   'drizzle-orm/sqlite-core';
 
-export const userT = sqliteTable(  'user', 
+const  userT  =sqliteTable( 'user', 
   {
 	id: text('id').primaryKey().notNull(),
-
     password: text('password').notNull(),
-
 	name: text('name').notNull(),
-
 	email: text('email').notNull().unique(),
-    
     accessAt: text('access_at').default(sql`CURRENT_TIMESTAMP`),
-
 	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
   },  
   (user)=> ({uid: uniqueIndex('uid').on(user.name)})
 ); // (arrow return statement) 
 
-export const  sessionT = sqliteTable(  'session', 
+const  sessionT  =sqliteTable( 'session', 
   {
     id: text('id').primaryKey().notNull(),
-
     userId: text('user_id').notNull().references(()=> userT.id),
-    
     expiresAt: integer('expires_at').notNull()
 });
-
-export type UserInsertSchema = typeof userT.$inferInsert;
