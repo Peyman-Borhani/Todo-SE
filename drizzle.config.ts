c62328436ce3835ef5f87b2fd9505b6d106c4b53
peyman-borhani <peyman.borhani@gmail.com>
2024-05-24 19:17:46 +0330

    -Remove: driver in config => only for Turso, or net remote DBs access...

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   schema:   './src/lib/srv/db/schema.ts',
   out:      './src/lib/srv/db/out',
   dialect:  'sqlite',
-  //driver: 'turso',
-  dbCredentials: {url: './src/lib/srv/db/todo-se.db'}, //'localDB/sql.db' },
-    //connectionString: process.env.DB_URL,
+  //Just For Turso//  driver:   'turso',
+  dbCredentials: {url: './src/lib/srv/db/todo-se.db'},//Just Turso// , authtoken: ''  },
+  //connectionString: process.env.DB_URL,
   verbose: true,
-  strict: true,
+  strict: true
 });
