03ccfa9e666550f88bae63d5b4bf73018c26f1d9
peyman-borhani <peyman.borhani@gmail.com>
2024-06-20 00:58:50 +0330

    +Refactor => auth landing pages

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,28 +1,25 @@
-import { lucia } from "$lib/server/auth";
-import { fail, redirect } from "@sveltejs/kit";
+import  {lucia}             from  '$lib/server/auth';
+import  {fail, redirect}    from  '@sveltejs/kit';
 
-import type { Actions, PageServerLoad } from "./$types";
+import type {Actions, 
+             PageServerLoad} from './$types';
 
-export const load: PageServerLoad = async (event) => {
-	if (!event.locals.user) {
-		return redirect(302, "/login");
-	}
-	return {
-		user: event.locals.user
-	};
-};
+export const load: PageServerLoad = 
+        async (ev)=>    (!ev.locals.user) ? redirect(302, '/login')
+                                          : {user: ev.locals.user};
 
 export const actions: Actions = {
-	default: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		await lucia.invalidateSession(event.locals.session.id);
+	default: async (ev)=> {
+		if (!ev.locals.session) return fail(401);
+
+		await lucia.invalidateSession(ev.locals.session.id);
 		const sessionCookie = lucia.createBlankSessionCookie();
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: ".",
-			...sessionCookie.attributes
+		ev.cookies.set( sessionCookie.name,
+                        sessionCookie.value, {
+			            path: '.',
+			            ...sessionCookie.attributes
 		});
-		return redirect(302, "/login");
+        
+		return  redirect(302, '/login');
 	}
 };
