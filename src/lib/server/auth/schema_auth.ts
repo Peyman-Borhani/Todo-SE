cbf60816087ef3a740e2cd6b9ea227890fb33ea6
peyman-borhani <peyman.borhani@gmail.com>
2024-06-20 01:08:10 +0330

    +intro: server auth +Add: schema_auth.ts => schema for authentication

---
+++ src/lib/server/auth/schema_auth.ts
@@ -0,0 +1,25 @@
+export  {userT,  sessionT}
+export type  UserInsertSchema  =typeof userT.$inferInsert;
+
+import  {sql}   from 'drizzle-orm';
+import  {integer,  sqliteTable,  
+         text,     uniqueIndex}  from   'drizzle-orm/sqlite-core';
+
+const  userT  =sqliteTable(  'user', 
+  {
+	id: text('id').primaryKey().notNull(),
+	name: text('name').notNull(),
+	email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+    accessAt: text('access_at').default(sql`CURRENT_TIMESTAMP`),
+	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
+  },  
+  (user)=> ({uid: uniqueIndex('uid').on(user.name)})
+); // (arrow return statement) 
+
+const  sessionT  =sqliteTable(  'session', 
+  {
+    id: text('id').primaryKey().notNull(),
+    userId: text('user_id').notNull().references(()=> userT.id),
+    expiresAt: integer('expires_at').notNull()
+});
