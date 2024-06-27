aabc346b157c4060e0789496cae56d5b2bf502d1
peyman-borhani <peyman.borhani@gmail.com>
2024-06-27 12:00:00 +0330

    {dev} >Replace:   svelte env => mockup env {build} >Replace: svelte env=> dotenvx +Add: env => to dbcredentials (correct way)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,23 @@
 import  {defineConfig}  from  'drizzle-kit';
-//import  {ENP}      from    '$env/static/private';
+//import  {env}      from    '$env/static/private';
+const env = {DB_URL: '0.0.0.0:8080',  DB_Token: 'a_B-c_D'}
 
 export default defineConfig({
-  schema:   './src/lib/srv/db/schema.ts',
-  out:      './src/lib/srv/db/out',
+  schema:   './src/lib/server/db/schema_DB.ts',
+  out:      './src/lib/server/db/out',
   dialect:  'sqlite',
   //Just For Turso//  driver:   'turso',
-  dbCredentials: {url: './src/lib/srv/db/todo-se.db'},//Just Turso// , authtoken: ''  },
-  //connectionString: process.env.DB_URL,
+  dbCredentials: {url : env.DB_URL,  authToken: env.DB_Token},
+//'./src/lib/server/db/todo-se.db'//Just Turso// , authtoken: ''  },
+//connectionString: DB_URL, //process.env.DB_URL,
   verbose: true,
-  strict: true
+  strict : true
 });
+
+/*
+DB_Pass : '_AB_CD_',
+DB_HOST : '0.0.0.0',
+DB_PORT : '8080',
+DB_USER : 'Arya',
+DB_PASS : 'Maxim'
+*/
