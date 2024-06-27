c9415038aa7754dde2ff05799564ff39802296d6
peyman-borhani <peyman.borhani@gmail.com>
2024-06-27 12:30:08 +0330

    +Fix +Change: DB folder => Move data DB files out of auth (seperation)

--- src/lib/server/DB_tools_.server.ts
+++ src/lib/server/DB_tools_.server.ts
@@ -3,7 +3,8 @@ export  {email_Exist, add_User}
 
 import  {eq}    from 'drizzle-orm';
 import  {DB}    from './DB_.server';
-import  {userT, type UserInsertSchema } from './schema_auth.ts';
+import  {userT, type UserInserType} //, type UserSelecType}
+                from './db/schema_DB.ts';
 
 const   email_Exist =
         async (email: string)=> 
@@ -13,14 +14,15 @@ const   email_Exist =
             .then(res=> res.length > 0);
 
 const  add_User =
-        async(usr:UserInsertSchema)=> await DB.insert(userT).values(usr);
+        async(usr:UserInserType)=> await DB.insert(userT).values(usr);
 
+//const result: User[] = db.select().from(users).all();
 export  const   getAllUsers  =
                 async()=> await DB.select(
-                            {id: userT.id,
-                            name: userT.name,
-                            email: userT.email
-                            }).from(userT);
-
+                            {   id: userT.id,
+                                name: userT.name,
+                                email: userT.email
+                            } ).from(userT).all();
+                                //remove .all() if issue
 export  const   deleteAllUsers =async()=> await DB.delete(userT);
 
