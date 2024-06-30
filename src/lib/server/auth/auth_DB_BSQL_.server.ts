5dd1b094710d79f2d7f14cd4a2357a71a29f5251
peyman-borhani <peyman.borhani@gmail.com>
2024-06-30 22:30:54 +0330

    +Add: auth_DB_BSQL3 => Single file BSQL3 auth, (to test / just in case...)

---
+++ src/lib/server/auth/auth_DB_BSQL_.server.ts
@@ -0,0 +1,35 @@
+// Full auth single file
+// BSQL3 temporarily
+import {DrizzleSQLiteAdapter}  from  '@lucia-auth/adapter-drizzle';
+//import  {createClient}  from    '@libsql/client';
+import  {drizzle}       from    'drizzle-orm/better-sqlite3';
+import  {sql}           from    'drizzle-orm';
+import  {sqliteTable, text, 
+         uniqueIndex, integer}  from  'drizzle-orm/sqlite-core';
+
+import  Database    from  'better-sqlite3';
+//const  sqlite =createClient({url: "http://127.0.0.1:8080"});//(':memory:'); //file: './src/lib/srv/db/todo-se.db'
+
+const  sql_DB = Database(':memory:');
+const  DB       = drizzle(sql_DB);
+
+const  userT  =sqliteTable( 'user', 
+{
+    id      : text('id').notNull().primaryKey(),
+    name    : text('name').notNull(),
+    password: text('password').notNull(),
+    accessAt: text('access_at').notNull().default(sql`CURRENT_TIMESTAMP`),
+    createdAt: text('created_at').notNull().default(sql`CURRENT_TIMESTAMP`)
+},  
+(user)=> ({uid: uniqueIndex('uid').on(user.name)})
+); // (arrow return statement) 
+
+
+const  sessionT  =sqliteTable( 'session', 
+{   
+    id:     text('id').notNull().primaryKey(),
+    userId: text('user_id').notNull().references(()=>userT.id),
+	expiresAt: integer('expires_at').notNull()
+});
+
+const adapter =new DrizzleSQLiteAdapter(DB, sessionT, userT);
