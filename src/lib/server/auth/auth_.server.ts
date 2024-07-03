0007a497a11c0c9925baa493dac3675a975e485d
peyman-borhani <peyman.borhani@gmail.com>
2024-07-03 12:21:28 +0330

    -Remove: createClient DB =>(+DRY)  same thing imported as DB +code => refactoring

--- src/lib/server/auth/auth_.server.ts
+++ src/lib/server/auth/auth_.server.ts
@@ -1,20 +1,18 @@
-// main auth libSQL client - lucia + drizzle 
+export  {lucia}
+// main auth file - libSQL client - lucia + drizzle 
 import  {dev}   from  '$app/environment';
 import  {DB}    from  '../DB_.server.ts';
-//import  {createClient}  from  '@libsql/client';
-import  {Lucia}         from  'lucia';
+import  {Lucia} from  'lucia';
 import  {userT, sessionT}     from './schema_auth.ts';
 import  {DrizzleSQLiteAdapter}from '@lucia-auth/adapter-drizzle';
 
-//const  sqlite =createClient({url: "http://127.0.0.1:8080"});//(':memory:'); //file: './src/lib/srv/db/todo-se.db'
 const  adapter  =new DrizzleSQLiteAdapter(DB, sessionT, userT);
 
-export const  lucia  =new Lucia(adapter, 
-  {
-	sessionCookie: {attributes: {secure: !dev}},
-	getUserAttributes: (attributes)=> 
-        ({name: attributes.name, email: attributes.email})
-  }
+const  lucia  = new Lucia(
+    adapter, 
+    { sessionCookie: {attributes: {secure: !dev}},
+	  getUserAttributes: atr=> ({name: atr.name, email: atr.email})
+    }
 );
 
 declare module  'lucia' {
@@ -25,3 +23,4 @@ declare module  'lucia' {
                             email: string}
   }
 }
+
