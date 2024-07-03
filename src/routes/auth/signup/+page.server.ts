00b0c17c1e1c9b98236ec85b3ff170b8cd685e50
peyman-borhani <peyman.borhani@gmail.com>
2024-07-03 12:26:05 +0330

    +code: refactor

--- src/routes/auth/signup/+page.server.ts
+++ src/routes/auth/signup/+page.server.ts
@@ -1,66 +1,57 @@
-import { lucia } from "$lib/server/auth";
-import { fail, redirect } from "@sveltejs/kit";
-import { generateId } from "lucia";
-import { Argon2id } from "oslo/password";
-import { SqliteError } from "better-sqlite3";
-import { db } from "$lib/server/db";
+import  {lucia}     from '$lib/server/auth/auth_.server.ts';
+import  {db}        from '$lib/server/db';
+import  {Argon2id}  from 'oslo/password';
+import  {fail, redirect}from '@sveltejs/kit';
+import  {generateId}    from 'lucia';
+import  {SqliteError}   from 'libsql';
 
-import type { Actions, PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad }  from  './$types';
 
-export const load: PageServerLoad = async (event) => {
-	if (event.locals.user) {
-		return redirect(302, "/");
-	}
+export const load: PageServerLoad = async (ev) => {
+	if (ev.locals.user) return redirect(302, '/');
 	return {};
 };
 
-export const actions: Actions = {
-	default: async (event) => {
-		const formData = await event.request.formData();
-		const username = formData.get("username");
-		const password = formData.get("password");
-		if (
-			typeof username !== "string" ||
-			username.length < 3 ||
-			username.length > 31 ||
-			!/^[a-z0-9_-]+$/.test(username)
-		) {
-			return fail(400, {
-				message: "Invalid username"
-			});
-		}
-		if (typeof password !== "string" || password.length < 6 || password.length > 255) {
-			return fail(400, {
-				message: "Invalid password"
-			});
-		}
+export const  actions: Actions =
+    
+  {default: async (ev)=> {
+		const formData =await ev.request.formData();
+		const username =formData.get('username');
+		const password =formData.get('password');
+        
+        let msg: String =
+          ( typeof username !== 'string'||
+			username.length  < 3        ||
+			username.length  > 31       ||
+			!/^[a-z0-9_-]+$/.test(username))
+        ? 'user name'
+        :  (typeof password !== 'string'||
+            password.length  < 6        ||
+            password.length > 255       )
+        ? 'password'  : 'OK';
 
-		const hashedPassword = await new Argon2id().hash(password);
-		const userId = generateId(15);
+        if(msg!=='OK') return fail(400, {message: 'Invalid ' + msg});
+		
+        const hashedPassword =await new Argon2id().hash(password);
+		const userId =generateId(15);
 
 		try {
-			db.prepare("INSERT INTO user (id, username, password) VALUES(?, ?, ?)").run(
-				userId,
-				username,
-				hashedPassword
-			);
+			db.prepare('INSERT INTO user (id, username, password) VALUES(?, ?, ?)'
+            ).run(  userId,
+                    username,
+				    hashedPassword );
 
 			const session = await lucia.createSession(userId, {});
 			const sessionCookie = lucia.createSessionCookie(session.id);
-			event.cookies.set(sessionCookie.name, sessionCookie.value, {
-				path: ".",
-				...sessionCookie.attributes
-			});
-		} catch (e) {
-			if (e instanceof SqliteError && e.code === "SQLITE_CONSTRAINT_UNIQUE") {
-				return fail(400, {
-					message: "Username already used"
-				});
-			}
-			return fail(500, {
-				message: "An unknown error occurred"
-			});
+			ev.cookies.set( sessionCookie.name, 
+                            sessionCookie.value,
+                            {path: '.', ...sessionCookie.attributes}
+            );
+		}catch (e) {
+			if( e instanceof SqliteError && e.code==='SQLITE_CONSTRAINT_UNIQUE')
+			      return fail(400, {message: 'This username is already taken'});
+			else  return fail(500, {message: 'Unknown error occurred'});
 		}
-		return redirect(302, "/");
+		return redirect(302, '/');
 	}
 };
