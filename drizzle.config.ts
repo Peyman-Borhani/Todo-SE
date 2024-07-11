58188e6ae24dff9d7800cee31a1d96759a731829
peyman-borhani <peyman.borhani@gmail.com>
2024-07-11 08:46:10 +0330

    +Add +fix: process.env => drizzle.config.ts - Remove: extra code, comments ~Refactor: code
    +up: deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,31 +1,22 @@
 import  {defineConfig}  from  'drizzle-kit';
-//import  {env}      from    '$env/static/private';
-//import * as dotenv from 'dotenv';
-//import type { Config } from 'drizzle-kit';
+//import  {get as env}   from  '@dotenvx/dotenvx';// config/set/status/encrypt/decrypt...
 
-//dotenv.config();
-//const {DB_URL} =process.env;
-//if(!DB_URL) throw new Error(
-//    'DB_URL not defined. Check environment variable(.env file)');
+const DB_TOKEN  =process.env.DB_TOKEN,
+      DB_URL    =process.env.DB_URL;
 
-const env = {DB_URL: 'http://127.0.0.1:8080',  DB_Token: 'a_B-c_D'}
+//const DB_TOKEN=env('DB_TOKEN'), DB_URL=env('DB_URL');
+//const {DB_TOKEN, DB_URL} = process.env; //console.log(DB_TOKEN, DB_URL)
+
+if(!DB_URL) throw new Error(
+    'DB_URL not defined. Check environment variable(.env file)');
+
 
 export default defineConfig({
   schema:   './src/lib/server/db/schema_DB.ts',
   out:      './src/lib/server/db/out',
   dialect:  'sqlite',
-  //Just For Turso//  driver:   'turso',
-  dbCredentials: {url : env.DB_URL,  authToken: env.DB_Token},
-//'./src/lib/server/db/todo-se.db'//Just Turso// , authtoken: ''  },
-//connectionString: DB_URL, //process.env.DB_URL,
+  dbCredentials: {url: DB_URL,  authToken: DB_TOKEN},
   verbose: true,
   strict : true
 });
-
-/*
-DB_Pass : '_AB_CD_',
-DB_HOST : '0.0.0.0',
-DB_PORT : '8080',
-DB_USER : 'Arya',
-DB_PASS : 'Maxim'
-*/
+//'./src/lib/server/db/todo-se.db'
