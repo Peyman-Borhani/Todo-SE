659aee8e345deb79a1e748a51e3037b773858d99
peyman-borhani <peyman.borhani@gmail.com>
2024-07-12 21:12:04 +0330

    +Add: data table => in schema +Rename: table => user -> users

--- src/lib/server/db/schema_DB.ts
+++ src/lib/server/db/schema_DB.ts
@@ -1,25 +1,39 @@
-export  {userT,  sessionT}
-export type  UserInserType  =typeof userT.$inferInsert;
-export type  UserSelecType = typeof userT.$inferSelect // return type when queried
+export  {usersT, dataT, sessionT}
+export type  UserInserType  =typeof usersT.$inferInsert;
+export type  UserSelecType = typeof usersT.$inferSelect // return type when queried
 
 import  {sql}   from 'drizzle-orm';
 import  {integer,  sqliteTable,  
          text,     uniqueIndex}  from   'drizzle-orm/sqlite-core';
 
-const  userT  =sqliteTable(  'user', 
+
+const  usersT  =sqliteTable(  'users', 
   {
-	id: text('id').primaryKey().notNull(),
+	id: text('id').primaryKey().notNull().unique(),
 	name: text('name').notNull(),
 	email: text('email').notNull().unique(),
     accessAt: text('access_at').default(sql`CURRENT_TIMESTAMP`),
 	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
   },  
-  (user)=> ({uid: uniqueIndex('uid').on(user.name)})
+  (users)=> ({uid: uniqueIndex('uid').on(users.name)})
+); // (arrow return statement)
+
+
+const  dataT  =sqliteTable(  'data', 
+  {
+	id: text('id').primaryKey().notNull().unique(),
+	name: text('name'),
+	data: text('data').notNull(),
+    accessAt: text('access_at').default(sql`CURRENT_TIMESTAMP`),
+	createdAt: text('created_at').default(sql`CURRENT_TIMESTAMP`)
+  },  
+  (data)=> ({uid: uniqueIndex('uid').on(data.id)})
 ); // (arrow return statement) 
+
 
 const  sessionT  =sqliteTable(  'session', 
   {
     id: text('id').primaryKey().notNull(),
-    userId: text('user_id').notNull().references(()=> userT.id),
+    userId: text('user_id').notNull().references(()=> usersT.id),
     expiresAt: integer('expires_at').notNull()
 });
