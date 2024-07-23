fdad44d929c207b3e87d4081664a6221394c927a
peyman-borhani <peyman.borhani@gmail.com>
2024-07-23 15:49:00 +0330

    Add: DB tool functions => get_User, get_all_Users. ~refactore updates.

--- src/lib/server/DB_tools_.server.ts
+++ src/lib/server/DB_tools_.server.ts
@@ -1,28 +1,36 @@
 
-export  {email_Exist, add_User}
+export  {email_Exist, add_User, get_User, get_all_Users}
 
 import  {eq}    from 'drizzle-orm';
 import  {DB}    from './DB_.server';
-import  {userT, type UserInserType} //, type UserSelecType}
-                from './db/schema_DB.ts';
+import  {type  UserSelecType, 
+         type  UserInserType, usersT
+        }       from './db/schema_DB.ts';
 
-const   email_Exist =
-        async (email: string)=> 
-            await  DB.select({email: userT.email}
-                    ).from(userT
-                    ).where(eq(userT.email, email))
-            .then(res=> res.length > 0);
+const  email_Exist=
+                async (email: string)=> 
+                await  DB.select({email: usersT.email}
+                ).from(usersT).where(eq(usersT.email, email))
+                 .then(res=> res.length > 0);
 
-const  add_User =
-        async(usr:UserInserType)=> await DB.insert(userT).values(usr);
+const  add_User=
+                async(usr: UserInserType)=> 
+                  await DB.insert(usersT).values(usr);
 
 //const result: User[] = db.select().from(users).all();
-export  const   getAllUsers  =
+const   get_User=
+                async(id: string)=> await DB.select(
+                            { id: usersT.id,
+                              name: usersT.name,
+                              email: usersT.email }
+                ).from(usersT).where(eq(usersT.id, id))
+                            
+const   get_all_Users=
                 async()=> await DB.select(
-                            {   id: userT.id,
-                                name: userT.name,
-                                email: userT.email
-                            } ).from(userT).all();
+                            { id: usersT.id,
+                              name: usersT.name,
+                              email: usersT.email }
+                ).from(usersT).all();
                                 //remove .all() if issue
-export  const   deleteAllUsers =async()=> await DB.delete(userT);
+const   del_all_Users =async()=> await DB.delete(usersT);
 
