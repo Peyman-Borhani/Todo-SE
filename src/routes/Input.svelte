fad8d6baf299ac24d784aafa3982c964519f8abb
peyman-borhani <peyman.borhani@gmail.com>
2024-07-23 13:10:10 +0330

    +Update: fix props => Runes correction +Add:  $props scoped in => single object  binding. +code: refactoring

--- src/routes/Input.svelte
+++ src/routes/Input.svelte
@@ -1,25 +1,24 @@
 <script>
    //import  {Date}  from  'svelte/reactivity';
+   import  {SvelteMap as sMap}  from  'svelte/reactivity';
+  import   {fade}     from   'svelte/transition';
 
-    import   {fade}     from   'svelte/transition';
-    //import { Data } from './store.svelte';
-        
-    let        
-            k = '',     // current Keyboard key press
-            prvKey,     // previous Keyboard key press 
-            focus = 0,
-            inp_el,
-            ISO  =false; // Local/ISO Time switch
-    // App/Menu kb control props
-    let  {menu  =$bindable(false),  info =$bindable(false), L = $bindable(true),// L means Keyboard focus on left side (todo)
-          zoom  =$bindable(false),  view =$bindable(false)
-    } = $props();
-   
+ // App/Menu kb control props  (i. => interactive input/settings)  
+   let  {i = $bindable({menu:false, info:false, L:true, zoom:false, view:false, focus:1})} = $props();
+//$inspect(input.menu);  // L? user focused on left side (todos)
+
+  let   k = '',     // current Keyboard key press
+        prvKey,     // previous Keyboard key press 
+        inp_el,     //focus = 0,
+        ISO  =false; // Local/ISO Time switch
+    
     let     time =  $state(' ');
-    const   tm  =()=> time = ISO? new Date().toISOString().slice(0,19).replace('T', '  ') 
-                                : new Date().toLocaleString().slice(0,22);
 
-setInterval(tm, 1000);
+    const   tmSW  =()=>  time =
+                ISO ? new Date().toISOString().slice(0,19).replace('T', '  ') 
+                    : new Date().toLocaleString().slice(0,22);
+
+(function tmRun() {setInterval(tmSW, 1000);  return clearInterval(tmSW)})();
 //$: k = (time===0) ?  true  : false;
 //$: k = ((t+1)%10 === 0) ? true  : false;
 const log = (n, x=false)=>  console.log(n, x? x :'');
@@ -27,8 +26,8 @@ const log = (n, x=false)=>  console.log(n, x? x :'');
 
 //________input handler (event/KeyBoard)_________
 function  kb_Control(evk) {
-//log(evk)//if user is typing or empty list return.
-if(evk.target.id==='Typin' || evk.target.id==='task-name')  return; 
+  //log(evk)//if user is typing or empty list return.
+  if(evk.target.id==='Typin' || evk.target.id==='task-name')  return; 
   let   c=0,    // counter to check if any element left over
         k = evk.key,
         ctrl =(evk.ctrlKey || evk.metaKey) ? true  :false;
@@ -37,54 +36,54 @@ if(evk.target.id==='Typin' || evk.target.id==='task-name')  return;
   switch(k) { 
     
     case 'i':
-        if(info && !menu)  info=false; //info off
-        else if(!info && menu) {menu=false;   info=true} //menu off - info on
-        else menu=true; // menu visible.    //log(menu) 
+        if(i.info && !i.menu)  i.info=false; //info off
+        else if(!i.info && i.menu) {i.menu=false;   i.info=true} //menu off - info on
+        else i.menu=true; // menu visible.    //log(menu) 
         break;
 
     case 'ArrowLeft':  
     case 'ArrowRight':	// Left or Right keys pressed
-        focus = 0; 	  // focus is the selected element virtual index
-        L = (k==='ArrowLeft') ?   true   : false;
+        i.focus = 0; 	  // focus is the selected element virtual index
+        i.L = (k==='ArrowLeft') ?   true   : false;
         //reaching the first todo(L) or done(R)
-        if  (L)  while(todos[focus].done)  focus++;
-        else     while(!todos[focus].done) focus++;
+        if  (i.L)  while(todos[i.focus].done)  i.focus++;
+        else     while(!todos[i.focus].done) i.focus++;
         break;
 
     case 'ArrowUp':   //up
-        if(L) while( focus>0 && todos[focus-1].done  ) {focus--; c++} //todo
-        else  while( focus>0 && !todos[focus-1].done ) {focus--; c++} //done
+        if(i.L) while( i.focus>0 && todos[i.focus-1].done  ) {i.focus--; c++} //todo
+        else  while( i.focus>0 && !todos[i.focus-1].done ) {i.focus--; c++} //done
         if(c===todos.length) break; //if all is from opposite site
-        if(focus>0) focus--; //go up -1
-        else focus = todos.length-1;
+        if(i.focus>0) i.focus--; //go up -1
+        else i.focus = todos.length-1;
         break;
 
     case 'ArrowDown':  //down
-        if(focus+1===todos.length) focus = -1;
-        if(L) while(todos[focus+1].done)  {focus++; c++} //todo
-        else  while(!todos[focus+1].done) {focus++; c++} //done
-        if(c!==todos.length) focus++; //if all is not from opposite side
+        if(i.focus+1===todos.length) i.focus = -1;
+        if(i.L) while(todos[i.focus+1].done)  {i.focus++; c++} //todo
+        else  while(!todos[i.focus+1].done) {i.focus++; c++} //done
+        if(c!==todos.length) i.focus++; //if all is not from opposite side
         break;                                // go down +1
     
     case 'Delete':
-        remove(todos[focus]);
-        if (focus===0) {}
-        else if (focus > 0) focus--;
-        else if(focus+1 <= todos.length) focus++;
-        updateID(); focus = 0;
+        remove(todos[i.focus]);
+        if (i.focus===0) {}
+        else if (i.focus > 0) i.focus--;
+        else if(i.focus+1 <= todos.length) i.focus++;
+        updateID(); i.focus = 0;
         break;
         
     case '+':   
-            zoom = !zoom;  
-            document.documentElement.style.setProperty('--size', 'calc((4vh + 4vw)/2)'); 
-            save();  load();  
-            break; // pressed +
-                
-    case 'v':   view=!view;	 break;  //view button
+        i.zoom = !i.zoom;  
+        document.documentElement.style.setProperty('--size', 'calc((4vh + 4vw)/2)'); 
+        save();  load();  
+        break; // pressed +
+        
+    case 'v':   i.view=!i.view;	 break;  //view button
 
     case 'Enter':	 //if enter key pressed and keyboard focused	
-            if(focus>=0) todos[focus].done = 
-                (todos[focus].done) ?  false  : true;
+            if(i.focus>=0) todos[i.focus].done = 
+                (todos[i.focus].done) ?  false  : true;
             break;
     
     case 'F5':          //to save data press F5
@@ -94,7 +93,7 @@ if(evk.target.id==='Typin' || evk.target.id==='task-name')  return;
                 if(prvKey==='F9') load();   break;  
                 
     default:  
-            focus=-1;   inp_el.focus();
+            i.focus=-1;   inp_el.focus();
             break;
   }
   prvKey = k; //saves key, to remember as last key pressed.
@@ -108,7 +107,7 @@ const  add_Item= ev=>  {
                         k==='Enter' ?  insert(t.value)//insert(t.value) 
                       : t==='button'?  insert(inp_el.value)  :{};
 }
-const insert = i=> log('insert:', i)
+const insert = ins=> log('insert:', ins)
 </script>
 
 <!-- onmouseover|once = {()=> document.documentElement.requestFullscreen()} -->
