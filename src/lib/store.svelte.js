6a7c36fe32485677db662d1d8ca5915ee4385c51
peyman-borhani <peyman.borhani@gmail.com>
2024-07-25 06:28:45 +0330

    +Add:  initialized => check if existing project running. +Update: Speed: array push => instead of ...rest +Add: 'new': to init() => new empty project +improve => mutiple fixes & refactoring.

--- src/lib/store.svelte.js
+++ src/lib/store.svelte.js
@@ -4,22 +4,24 @@ import  {SvelteMap as sMap}  from  'svelte/reactivity';
 //const url  =process.env.DB_URL,
 //      token=process.env.DB_Token;   //'http://127.0.0.1:8080'; //'http://0.0.0.0:8080'
 //console.log(url, token);
-let qN= 1;  // que number
+let qN = 1,  // que number
+    initialized =  false;
 
 const time_Date = (t = new Date().toISOString())=> 
     ( { id: (t.slice(0,10)+' '+ t.slice(11, 23) ),
         date: t.slice(0,10),
         time: t.slice(11, 24)
     });
-// D.o, D.one Arrays  
+    
+// D.o, D.one Arrays  //loading? must placed in order in array
 const   D = { o:[],   one:[] };
 
-const   
-    def_todos =[ // default info if not initialized.
+const   // default demo mode data  (not loaded / not a new project).
+    def_todos =[ 
             'Check my messages',  'review the tasks',
             'Do few job orders',  'Solve few problems 🎯',
             'Code for 2 hours',   'Write a new blog post', 
-    ],
+    ],      
     def_dones =['Think of new ideas', 'Learn - practice - develop'];
 
 
@@ -44,9 +46,11 @@ function  make_Store()
                                    || inp==='' || inp===' ')
            {console.log('bad input at:  ', inp);  return}
       }
-        // Update D.o/D.one array  //remove if not needed
-        (typ==='todo')  ? D.o   =[inp, ...D.o]   
-                        : D.one =[inp, ...D.one];
+        // Add new records to D.o/D.one if project is live (initialized)
+        if(initialized) {typ==='todo' ?  D.o.push(inp)   
+                                      :  D.one.push(inp);
+        } 
+        console.log(D.o);
         // nake a unique id    // C.randomUUID();
         let id = URL.createObjectURL(new Blob([])).slice(-36); //36  
         
@@ -60,31 +64,34 @@ function  make_Store()
                                 ['tasks', false] ])  );
         URL.revokeObjectURL(id);
     }  
-    //_________initializing Data______________
+    //____initialize / load  Data __________ params type: Array
     function  init (todos=false,  dones=false){
-
-        //if(D.o.length<1 || D.one.length<1){ // "error: list initialized already";
-        //assign todos/dones params if valid, else fill with default values  
+      if( initialized && todos!=='new')  //if initialized
+         return("Dev Error: list initialized already, use: init('new') for new project");
+      qN = 1;  // resets que number
+      //if new project 
+      if(todos==='new') {D.o=[];   D.one=[]} // then returns
+      else  //assign todos/dones if params provided and valid,
+      {    //or fill with default values if no params passed in
         D.o   =Array.from( (Array.isArray(todos) && todos.length>0)
                             ? todos  :def_todos);
         D.one =Array.from( (Array.isArray(dones) && dones.length>0)
                             ? dones  :def_dones);
-        //} //qN = 1;
         D.one.forEach( dn=> insert(dn, 'done') );
         D.o.forEach(  td=> insert(td, 'todo') );
-        //for test: //Data.get(id).set('title', 'abcd')
-        //console.log('item: ', Data.get(id).get('title'));
+      }
+      initialized = true;
     }
     //___________________Data Tools_________________________
     const  
         remove =id=> Data.delete(id),
         mark   =id=> Data.get(id).set('done', !(Data.get(id).get('done')))
         ,
-        sort    = x=>  new Map([...x].sort()),
-        rev     = x=>  new Map([...x].reverse()),
+        sort    = x=>  new Map([...x.entries()].sort()),
+        rev     = x=>  new Map([...x.entries()].reverse()),
         // Using Map.groupBy to sort items based on specified category
-        sortBy  = (id, x)=> Map.groupBy(Data=> Data.get(id).get(x)),
-        log     = ()=> {for(let d of Data.keys()) console.log(Data.get(d))};
+        sortBy  = (id, x='time')=> Map.groupBy(Data=>  Data.get(id).get(x)),
+        log     = ()=> {for(let d of Data.keys()) console.log(Data.get(d).title)};
 
     return {  Data, //get Data()  {return  Data},
               init,  insert,  log,  sort,
